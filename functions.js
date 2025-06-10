5bb2fe75593f08d513ab98416d56a4367ae1df77
PONNA LOKESH KUMAR <lokeshkumarponna@gmail.com>
2025-06-10 19:02:13 +0530

    Adding code for Arrow Functions in Functions.js file

--- functions.js
+++ functions.js
@@ -10,3 +10,39 @@ let anonymousFunction = function (){
 }
 
 anonymousFunction();
+
+// ---> Arrow Function
+
+const sum1 = function (x,y) {
+    return x+y;
+}
+
+const sum2 = (x,y) => {
+    return x+y;
+}
+
+console.log(sum2(2,3));
+
+// or -->
+
+const sum3 = (x,y) => x+y;
+
+const Add = (x,y) => {
+    if(x>y){
+        return x+y;
+    }else{
+        return x-y;
+    }
+}
+
+console.log(Add(3,5));
+
+//Or -->
+
+const Add1 = (x,y) => x>y? x+y : x-y;
+
+console.log(Add1(5,3));
+
+
+
+
