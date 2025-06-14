5069a224ac1e15456f17641f7756b4269e3ec220
PONNA LOKESH KUMAR <lokeshkumarponna@gmail.com>
2025-06-14 19:21:44 +0530

    Added more files to Modules Folder

---
+++ Modules Folder/index.js
@@ -0,0 +1,15 @@
+
+export const calculate = (a,b) => {
+    console.log(a+b);
+}
+
+export function greet() {
+    console.log("Hello Everyone");
+}
+
+
+// --> Modules exporting to other files
+// module.exports = {
+//     calculate,
+//     greet
+// }
---
+++ Modules Folder/utils.js
@@ -0,0 +1,9 @@
+import { greet, calculate } from "./index.js";
+
+greet();
+
+calculate(5,3);
+
+// const allTheModules = require("./index.js");
+
+// allTheModules.greet();
